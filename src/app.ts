1c0a1cb1bdc9b49411b377672bfda2e42d350986
imadnanhassan <abulhassan.dev@gmail.com>
2024-11-23 03:23:03 +0600

    gg

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '250mb' }));
 app.use(urlencoded({ extended: true }));
 
 // Simple route
